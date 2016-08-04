65547f26cde34937d96159af78913157eeefa8f9
smaxwellstewart <smaxwellstewart@gmail.com>
2016-08-04 10:35:37 -0400

    fixed mock laozi print

--- mock.go
+++ mock.go
@@ -5,9 +5,9 @@ import "fmt"
 type MockLaozi struct{}
 
 func (d MockLaozi) Log(b []byte) {
-	fmt.Println("Mock Laozi output", b)
+	fmt.Printf("[laozi] event logged: %s\n", b)
 }
 
 func (d MockLaozi) Close() {
-	fmt.Println("Mock Laozi closing")
+	fmt.Println("[laozi] closing!")
 }
